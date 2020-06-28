7a23588e99a6bc38bd1b3cb531be57064b7e90e6
swsoyee <swsoyee@gmail.com>
2020-06-29 00:23:47 +0900

    style: use icon instead string symbol

--- src/atoms/Square.tsx
+++ src/atoms/Square.tsx
@@ -1,18 +1,34 @@
 import * as React from 'react';
 import { Box, Button } from '@material-ui/core';
 
+import RadioButtonUncheckedIcon from '@material-ui/icons/RadioButtonUnchecked';
+import CloseIcon from '@material-ui/icons/Close';
+
 const Square: React.FC<{
     onClick: () => void,
     value: string,
 }> = (props) => {
     const { onClick, value } = props;
+    let symbol;
+    if (value) {
+        if (value === 'O') {
+            symbol = <RadioButtonUncheckedIcon />;
+        } else if (value === 'X') {
+            symbol = <CloseIcon />;
+        } else {
+            symbol = value;
+        }
+    } else {
+        symbol = '　';
+    }
+
     return (
         <Box component="span" m={0}>
             <Button
                 variant="outlined"
                 onClick={onClick}
             >
-                {value || '　'}
+                {symbol}
             </Button>
         </Box>
     );
